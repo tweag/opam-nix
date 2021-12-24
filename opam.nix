99dbcfb6d9e0cb18d9945d9ea252fdf833885fa8
Alexander Bantyev <balsoft@balsoft.ru>
2021-12-24 23:06:02 +0300

    Prevent package dependencies on repos

--- opam.nix
+++ opam.nix
@@ -96,6 +96,12 @@ in rec {
 
     in lines solution;
 
+  listToAttrsBy = by: list: listToAttrs (map (x: nameValuePair x.${by} x) list);
+
+  contentAddressedIFD = dir:
+    builtins.deepSeq (readDir dir)
+    (/. + builtins.unsafeDiscardStringContext dir);
+
   makeOpamRepo = dir:
     let
       files = readDirRecursive dir;
@@ -103,11 +109,11 @@ in rec {
         (name: value: isAttrs value || hasSuffix "opam" name) files;
       opamFilesOnly =
         converge (filterAttrsRecursive (_: v: v != { })) opamFiles;
-      packages = collect isList (mapAttrsRecursive (path: _:
-        let
-          fileName = lib.last path;
-          dirName = splitNameVer (lib.last (lib.init path));
-          parsedOPAM = fromOPAM "${dir + ("/" + concatStringsSep "/" path)}";
+      packages = concatLists (collect isList (mapAttrsRecursive
+        (path': _: [rec {
+          fileName = lib.last path';
+          dirName = splitNameVer (lib.last (lib.init path'));
+          parsedOPAM = fromOPAM opamFile;
           name = parsedOPAM.name or (if hasSuffix ".opam" fileName then
             removeSuffix ".opam" fileName
           else
@@ -117,41 +123,43 @@ in rec {
             dirName.version
           else
             "local");
-        in [
-          {
-            name = "sources/${name}/${name}.${version}";
-            path = "${dir + ("/" + concatStringsSep "/" (lib.init path))}";
-          }
-          {
-            name = "packages/${name}/${name}.${version}/opam";
-            path = "${dir + ("/" + concatStringsSep "/" path)}";
-          }
-        ]) opamFilesOnly);
+          source = dir + ("/" + concatStringsSep "/" (lib.init path'));
+          opamFile = dir + ("/" + (concatStringsSep "/" path'));
+        }]) opamFilesOnly));
       repo-description = {
         name = "repo";
         path = toFile "repo" ''opam-version: "2.0"'';
       };
-      repo = pkgs.linkFarm "opam-repo"
-        ([ repo-description ] ++ concatLists packages);
-    in repo;
+      opamFileLinks = map ({ name, version, opamFile, ... }: {
+        name = "packages/${name}/${name}.${version}/opam";
+        path = opamFile;
+      }) packages;
+      sourceMap = lib.foldl (acc: x:
+        lib.recursiveUpdate acc { ${x.name} = { ${x.version} = contentAddressedIFD x.source; }; }) { }
+        packages;
+      repo = pkgs.linkFarm "opam-repo" ([ repo-description ] ++ opamFileLinks);
+    in repo // { passthru.sourceMap = sourceMap; };
 
-  queryToDefs = repo: packages:
+  queryToDefs = repos: packages:
     let
       findPackage = name: version:
         let
-          sourcePath = "${repo}/sources/${name}/${name}.${version}";
-          isLocal = pathExists sourcePath;
-          pkgDir = "${repo}/packages/${name}/${name}.${version}";
-          filesPath = "${pkgDir}/files";
+          pkgDir = repo: repo + "/packages/${name}/${name}.${version}";
+          filesPath = contentAddressedIFD (pkgDir repo + "/files");
+          repo = head (filter (repo: pathExists (pkgDir repo)) repos);
+          isLocal = repo ? passthru.sourceMap;
         in {
-          opamFile = "${pkgDir}/opam";
+          opamFile = pkgDir repo + "/opam";
           inherit name version isLocal repo;
           src = if isLocal then
-            pkgs.runCommand "source-copy" { }
-            "cp --no-preserve=all -R ${sourcePath}/ $out"
+            pkgs.runCommand "source-copy" { } "cp --no-preserve=all -R ${
+              repo.passthru.sourceMap.${name}.${version}
+            }/ $out"
           else
             pkgs.emptyDirectory;
-        } // optionalAttrs (pathExists filesPath) { files = filesPath; };
+        } // optionalAttrs (pathExists (pkgDir repo + "/files")) {
+          files = filesPath;
+        };
 
       packageFiles = mapAttrs findPackage packages;
     in mapAttrs
@@ -182,7 +190,7 @@ in rec {
     pipe query [
       (opamList repo)
       (opamListToQuery)
-      (queryToDefs repo)
+      (queryToDefs repos)
       (defsToScope pkgs)
       (applyOverlays overlays)
     ];
