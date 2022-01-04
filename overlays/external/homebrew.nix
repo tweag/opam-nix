486b16937a3e702d8e255a54ade7bed30894b1c1
Alexander Bantyev <balsoft@balsoft.ru>
2022-01-04 15:29:29 +0300

    Add autoconf to homebrew map

--- overlays/external/homebrew.nix
+++ overlays/external/homebrew.nix
@@ -4,6 +4,14 @@ with pkgs;
 let
 
 in {
+  "autoconf" = pkgsBuildBuild.autoconf;
+  "cairo" = cairo.dev;
+  "expat" = expat.dev;
   "gnupg" = gnupg;
+  "gmp" = gmp.dev;
+  "gtk+3" = gtk3.dev;
+  "gtksourceview3" = gtksourceview3.dev;
+  "libxml2" = libxml2.dev;
   "pkg-config" = pkg-config;
+  "zlib" = zlib.dev;
 }
--- overlays/ocaml-darwin.nix
+++ overlays/ocaml-darwin.nix
@@ -2,11 +2,17 @@ final: prev:
 let
   inherit (prev.nixpkgs) lib;
 
+  pkgs = prev.nixpkgs;
+
   inherit (import ./lib.nix lib) applyOverrides;
 
   overrides = rec {
     ocurl = oa: {
-      buildInputs = oa.buildInputs ++ [ prev.nixpkgs.curl.dev ];
+      buildInputs = oa.buildInputs ++ [ pkgs.curl.dev ];
+    };
+
+    conf-which = oa: {
+      nativeBuildInputs = oa.nativeBuildInputs ++ [ pkgs.which ];
     };
   };
 in applyOverrides prev overrides
