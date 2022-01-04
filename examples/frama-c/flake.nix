a25ed40b88b39d6ebafdc17fd38cf4a837f56679
Alexander Bantyev <balsoft@balsoft.ru>
2022-01-04 15:41:14 +0300

    frama-c: don't disable lablgtk

--- examples/frama-c/flake.nix
+++ examples/frama-c/flake.nix
@@ -14,16 +14,13 @@
 
         scope = queryToScope { } {
           frama-c = null;
-          lablgtk3 = null;
+          lablgtk3 = null; # Use lablgtk3 when appropriate
           lablgtk3-sourceview3 = null;
-          conf-gtksourceview = null;
           ocaml-base-compiler = "4.12.0";
         };
 
         overlay = self: super: {
           # opam is adamant about using gtk2 :/
-          lablgtk = null;
-
           frama-c = super.frama-c.overrideAttrs (oa: {
             nativeBuildInputs = oa.nativeBuildInputs ++ [ pkgs.makeWrapper ];
 
