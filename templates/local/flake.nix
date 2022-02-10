134c36d403253635b6a67db0990ccbbcc384953b
Alexander Bantyev <balsoft@balsoft.ru>
2022-02-10 22:34:07 +0300

    Fix local template

--- templates/local/flake.nix
+++ templates/local/flake.nix
@@ -2,13 +2,15 @@
   inputs = {
     opam-nix.url = "github:tweag/opam-nix";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
   };
-  outputs = { self, flake-utils, opam-nix }@inputs:
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
     let package = throw "Put the package name here!";
     in flake-utils.lib.eachDefaultSystem (system: {
       legacyPackages = let
-        inherit (opam-nix.lib.${system}) buildOpamProject;
-        scope = buildOpamProject { } ./. { };
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        scope = on.buildOpamProject { } package ./. { };
         overlay = self: super:
           {
             # Your overrides go here
