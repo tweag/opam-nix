tweag/opam-nix

commit 5486c7425c5e60a6836ee8778cd5a4cd861e64db

Fix multi-package and executable templates

templates/executable/flake.nix
@@ -25,7 +25,7 @@
           ## - or force ocamlfind to be a certain version:
           # ocamlfind = "1.9.2";
         };
-        scope = on.buildOpamProject { } package ./. query;
+        scope = on.buildOpamProject' { } package ./. query;
         overlay = final: prev: {
           # You can add overrides here
           ${package} = prev.${package}.overrideAttrs (_: {
@@ -37,8 +37,8 @@
         # The main package containing the executable
         main = scope'.${package};
         # Packages from devPackagesQuery
-        devPackages =
-          pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope';
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
       in {
         legacyPackages = scope';
 

templates/multi-package/flake.nix
@@ -25,15 +25,15 @@
           ## - or force ocamlfind to be a certain version:
           # ocamlfind = "1.9.2";
         };
-        scope = on.buildOpamProject { } ./. query;
+        scope = on.buildOpamProject' { } ./. query;
         overlay = final: prev:
           {
             # You can add overrides here
           };
         scope' = scope.overrideScope' overlay;
         # Packages from devPackagesQuery
-        devPackages =
-          pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope';
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
         # Packages in this workspace
         packages =
           pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope';

commit 209a0a86192c92c45a8d684cf68f33143e019904

Fix executable template use of `buildOpamProject'`
It's currently passing `package` the first parameter as the `name` required for the `buildOpamProject` function.

This error was introduced in 5486c74

templates/executable/flake.nix
@@ -25,7 +25,7 @@
           ## - or force ocamlfind to be a certain version:
           # ocamlfind = "1.9.2";
         };
-        scope = on.buildOpamProject' { } package ./. query;
+        scope = on.buildOpamProject' { } ./. query;
         overlay = final: prev: {
           # You can add overrides here
           ${package} = prev.${package}.overrideAttrs (_: {