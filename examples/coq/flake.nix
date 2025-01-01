6e6a1ac0203c3595c1ffac129d9ac32d8f7989c1
Alexander Bantyev <balsoft@balsoft.ru>
2025-01-01 22:17:45 +0400

    Fix the coq example
    Closes https://github.com/tweag/opam-nix/issues/106

--- examples/coq/flake.nix
+++ examples/coq/flake.nix
@@ -2,6 +2,7 @@
   description = "Build a coq package";
   inputs.opam-nix.url = "github:tweag/opam-nix";
   inputs.opam-repository.url = "github:ocaml/opam-repository";
+  inputs.opam-repository.flake = false;
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, opam-nix, flake-utils, opam-repository, nixpkgs }:
