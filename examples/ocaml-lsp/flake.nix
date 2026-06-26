6f0af4c91dfa952d7a9797f7e7a8f652ac2d05c3
nikochiko <k+git@kaustubh.page>
2026-06-26 16:40:59 +0530

    fix: update ocaml-lsp & merlin lib rev to build

--- examples/ocaml-lsp/flake.nix
+++ examples/ocaml-lsp/flake.nix
@@ -16,14 +16,22 @@
           src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
             owner = "ocaml";
             repo = "ocaml-lsp";
-            rev = "c961c46fc4705b18d336ac990b9c3b39354b9d7b";
-            sha256 = "U7g2ilKfd8EES1EDgy46LKkG/z1jwpd5LIkkqhe13iI=";
+            rev = "b309c2e58dd0c9077271bb0204554d4694e26317";
+            sha256 = "/rtKoUqaNoBcqHGZ/qwSBQKnrqo7F+kNTEsqb3ZpkJs=";
             fetchSubmodules = true;
           };
           inherit (opam-nix.lib.${system}) buildOpamProject';
           scope = buildOpamProject' { } src { ocaml-base-compiler = "*"; };
+          merlinSrc = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+            owner = "ocaml";
+            repo = "merlin";
+            rev = "03c8e3f0cef88ead9f217c35af89523eeaf095d3";
+            sha256 = "sha256-QvLHseQN/KBDeAZ6Y4X1qCxx0OsoWIKVwfoZo6bLIOI=";
+          };
         in
-        scope;
+        scope.overrideScope (self: super: {
+          merlin-lib = super.merlin-lib.overrideAttrs (_: { src = merlinSrc; });
+        });
 
       packages.default = self.legacyPackages.${system}.ocaml-lsp-server;
     });
